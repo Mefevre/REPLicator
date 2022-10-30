1093e4b5691860522275e76b0e7347701de7ae01
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-10-30 12:02:43 +0100

    Ajustement colone

--- Sources/Center.sh
+++ Sources/Center.sh
@@ -2,5 +2,5 @@ center ()
 {
     termwidth="$(tput cols)";
     padding="$(printf '%0.1s' ={1..500})";
-    printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "  $1  " 0 "$(((termwidth-1-${#1})/2))" "$padding"
+    printf '%*.*s %s %*.*s\n' 0 "$((((termwidth-2-${#1})/2)-2))" "$padding" "  $1  " 0 "$((((termwidth-1-${#1})/2)-2))" "$padding"
 }

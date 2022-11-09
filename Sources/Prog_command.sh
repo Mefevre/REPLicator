51b8d09b2d4c777f8d8a28000c6e89ea59082cf7
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-11-09 19:24:05 +0100

    fix command LIST

--- Sources/Prog_command.sh
+++ Sources/Prog_command.sh
@@ -8,7 +8,7 @@ set() {
 show(){
     echo -e "Variable enregistrer dans cette environnement : "
     for i in ${!REPL_var[@]}; do
-        echo "  $i = ${tab["$i"]}"
+        echo "  $i = ${REPL_var["$i"]}"
     done
 }
 
@@ -17,25 +17,53 @@ exec() {
     //
 }
 
-# show tools installed and supported
-list () {
+# show tools installed and supported FIRT VUE
+# list () {
+#     update_soft_installer_all
+#     echo -e "list of compatible programs : "
+#     for i in ${!REPL_soft[@]}; do
+#         if [[ ${REPL_soft["$i"]} -eq 2 ]]; then
+#             echo "  $i = ${REPL_soft["$i"]}"
+#         fi
+#     done
+# }
+# SECOND VUE
+list() {
     update_soft_installer_all
-    for i in ${!REPL_soft[@]}; do
-        if [[ ${REPL_soft["$i"]} -eq 0 ]]; then
-            echo "  $i = ${REPL_soft["$i"]}"
-        fi
-    done
+    if [[ $# -gt 0 ]]; then
+        echo -e "This programs are installed : "
+        for i in $@; do
+            if [[ ${REPL_soft["$i"]} -eq 1 ]]; then
+                echo -e "   - $i               $GREEN[INSTALLED]$END"
+            fi
+        done
+    else
+        echo -e "list of compatible programs : "
+        for i in ${!REPL_soft[@]}; do
+            if [[ ${REPL_soft["$i"]} -ne 0 ]]; then
+                echo -n "   - $i    "
+                if [[ ${REPL_soft["$i"]} -eq 1 ]]; then
+                    echo -e -n "           $GREEN[INSTALLED]$END"
+                fi
+                echo ""
+            fi
+        done
+    fi
 }
 
 update_soft_installer_all() {
     for i in ${!REPL_soft[@]}; do
-        REPL_soft["$i"]=update_soft_installer $i
+        REPL_soft['$i']=$(update_soft_installer $i)
     done
 }
 
 update_soft_installer() {
     which $1
-    return $?
+    if [[ $? -eq 0 ]]; then
+        return 1
+    else
+        return 2
+    fi
 }
 
 # install tools supported

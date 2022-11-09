abc826ed966a026e673570608946db9b60fee19c
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-11-09 17:04:20 +0100

    update fonct CENTER

--- Sources/Center.sh
+++ Sources/Center.sh
@@ -2,5 +2,5 @@ center ()
 {
     termwidth="$(tput cols)";
     padding="$(printf '%0.1s' ={1..500})";
-    printf '%*.*s %s %*.*s\n' 0 "$((((termwidth-2-${#1})/2)-2))" "$padding" "  $1  " 0 "$((((termwidth-1-${#1})/2)-2))" "$padding"
+    echo -e $1 | printf '%*.*s %s %*.*s\n' 0 "$((((termwidth-2-${#1})/2)-2))" "$padding" "  $(cat /dev/stdin)  " 0 "$((((termwidth-1-${#1})/2)-2))" "$padding"
 }

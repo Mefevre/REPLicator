7f8ebc1fbeacc4cdcfba5be47401ac3ea7b9e422
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-11-24 14:19:49 +0100

    ajout d'un script qui install les dependances

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo -e "Installation des dépandances :"
+echo -e "       - figlet"
+sudo apt install  figlet
+echo -e "       - lolcat"
+sudo apt install  lolcat
+echo -e "       - python3"
+sudo apt install  python3
+echo -e "       - curl"
+sudo apt install curl

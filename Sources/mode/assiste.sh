16fe1e23b4b9f69eff19ced9ec94a6a06f0c5151
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-12-06 15:12:41 +0100

    gestion de DEL

--- Sources/mode/assiste.sh
+++ Sources/mode/assiste.sh
@@ -1,33 +1,11 @@
-assiste() {
-    echo -e "Action disponnibles :
-
-    1 - parametrer une variable
-    2 - supprimer une variable
-    3 - affichier toutes les variables
-    4 - lancer un programme
-    5 - lister les programmes installés
-    6 - installer un programme
-    7 - afficher l'aide
-    8 - changer de mode
-    9 - affichier la meteo local
-    10 - ...
-    q - quitter le programme
-    "
-
-    echo -e -n "${REPL_var['mode_color']}Entrée le numéro de votre choix $> $END"
-    read -a REPLY
-    echo "${REPLY[@]}" >> $REPL_script_path/Sources/.history
-    comm=${REPLY[0]}
-    unset REPLY[0]
-    case $comm in
-        1)
-            tmp=1
+control_var() {
+    tmp=1
             echo -e "Liste des variables disponnibles : "
             for i in ${!REPL_var[@]}; do
                 echo "      $tmp - $i"
                 tmp=$((tmp+1))
             done
-            echo -e -n "Entrée de la variable a parametrer : "
+            echo -e -n "Entrée le numéro de la variable : "
             read var
             case $var in
                 1)
@@ -60,10 +38,36 @@ assiste() {
                     echo -e "$ROSE Entree interdite !$END"
                     return 1
             esac
+}
+assiste() {
+    echo -e "Action disponnibles :
+
+    1 - parametrer une variable
+    2 - supprimer une variable
+    3 - affichier toutes les variables
+    4 - lancer un programme
+    5 - lister les programmes installés
+    6 - installer un programme
+    7 - afficher l'aide
+    8 - changer de mode
+    9 - affichier la meteo local
+    10 - ...
+    q - quitter le programme
+    "
+
+    echo -e -n "${REPL_var['mode_color']}Entrée le numéro de votre choix $> $END"
+    read -a REPLY
+    echo "${REPLY[@]}" >> $REPL_script_path/Sources/.history
+    comm=${REPLY[0]}
+    unset REPLY[0]
+    case $comm in
+        1)
+            control_var
             echo -e -n "Entrée la nouvelle valeur de cette variable : "
             read val
             set $var $val ;;
         2)
+            control_var
             del ${REPLY[@]} ;;
         3)
             show ;;

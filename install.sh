b24f95aebdba888cd828c2adac6f5ccfb6549a7e
Maskit <lefevre.nathan@etu.univ-lyon1.fr>
2022-12-08 23:50:11 +0100

    effacement des sortie APT du script install

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@ path="$(dirname $(readlink -f $0))"
 
 echo -e "Installation des dépandances :"
 echo -e "       - figlet"
-sudo apt install  figlet
+sudo apt install  figlet -y > /dev/null
 echo -e "       - lolcat"
-sudo apt install  lolcat
+sudo apt install  lolcat -y > /dev/null
 echo -e "       - python3"
-sudo apt install  python3
+sudo apt install  python3 -y > /dev/null
 echo -e "       - curl"
-sudo apt install curl
+sudo apt install curl -y > /dev/null
 
 echo -e "       - REPLicator"
 sudo mv $path /etc/
